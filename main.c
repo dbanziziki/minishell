f383a856162d88f9d7d88b12fc5842b94d6a6055
David Banziziki <dbanzizi@mi-r3-p4.s19.be>
2021-08-05 12:17:59 +0200

    parser

--- main.c
+++ main.c
@@ -3,16 +3,14 @@
 
 typedef enum e_states t_states;
 
-
 enum e_states
 {
-	WHITE_SPACE,
 	WORD,
-	FLAG,
 	REDIRECT,
+	UNKNOWN,
 	HEREDOC,
+	WHITE_SPACE = ' ',
 	PIPE = '|',
-	AND = '&',
 };
 
 typedef struct s_token
@@ -23,10 +21,27 @@ typedef struct s_token
 	struct s_token	*prev;
 }				t_token;
 
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (*((char *)s1 + i)
+		&& *((char *)s1 + i) == *((char *)s2 + i) && i < n - 1)
+		i++;
+	if (n)
+		return (*((unsigned char *)s1 + i) - *((unsigned char *)s2 + i));
+	return (0);
+}
 
 int		ft_isalpha(int c)
 {
 	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
+
+int		ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
 }
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
@@ -58,7 +73,7 @@ char	*ft_strndup(const char *s, size_t n)
 	return (res);
 }
 
-t_token	*new_tokenizer(t_states type, char *value)
+t_token	*new_token(t_states type, char *value)
 {
 	t_token	*new;
 
@@ -92,19 +107,43 @@ void	add_back(t_token **head, t_token *new)
 
 int	parser(t_token *tokens)
 {
-	if (tokens->type != WORD)
+	if (tokens->type != WORD && tokens->type != REDIRECT && tokens->type != HEREDOC)
 	{
 		printf("minishell: parse error near `%s`\n", tokens->value);
 		return (-1);
 	}
 	while (tokens)
+	{
+		if (tokens->type == WORD)
+		{
+			//code
+		}
+		else if (tokens->type == REDIRECT)
+		{
+			//code
+		}
+		else if (tokens->type == HEREDOC)
+		{
+			//code
+		}
+		else if (tokens->type == PIPE)
+		{
+			//code
+		}
+		else
+		{
+			printf("minishell: unexpected token near `%s`\n", tokens->value);
+			return (-1);
+		}
 		tokens = tokens->next;
+	}
+	return (1);
 }
 
 t_token	*get_tokens(char *str)
 {
-	int	i;
-	t_token	*head;
+	int			i;
+	t_token		*head;
 	char		*temp;
 	int			cursor;
 
@@ -113,36 +152,49 @@ t_token	*get_tokens(char *str)
 	while (str[++i])
 	{
 		cursor = -1;
-		if (str[i] == ' ')
+		if (str[i] == WHITE_SPACE)
 			continue ;
 		else if (str[i] == '|')	
-			add_back(&head, new_tokenizer(PIPE, ft_strndup(&str[i], 1)));
-		else if (str[i] == '&')
-			add_back(&head, new_tokenizer(AND, ft_strndup(&str[i], 1)));
+			add_back(&head, new_token(PIPE, ft_strndup(&str[i], 1)));
 		else if (str[i] == '<' || str[i] == '>')
-			add_back(&head, new_tokenizer(REDIRECT, ft_strndup(&str[i], 1)));
-		else if (ft_isalpha(str[i]))
+		{
+			if (!ft_strncmp(&str[i], "<<", 2))
+			{
+				add_back(&head, new_token(HEREDOC, ft_strndup(&str[i], 2)));
+				i++;
+				continue ;
+			}
+			else if (!ft_strncmp(&str[i], ">>", 2))
+			{
+				add_back(&head, new_token(REDIRECT, ft_strndup(&str[i], 2)));
+				i++;
+				continue ;
+			}
+			add_back(&head, new_token(REDIRECT, ft_strndup(&str[i], 1)));
+		}
+		else if (ft_isalpha(str[i]) || str[i] == '-' || ft_isdigit(str[i]))
 		{
 			temp = &str[i];
-			while (temp[++cursor]);
+			while (temp[++cursor])
 			{
 				if (temp[cursor] == ' ' || temp[cursor] == '|' || temp[cursor] == '&' || temp[cursor] == '<' || temp[cursor] == '>')
 					break ;
 			}
-			add_back(&head, new_tokenizer(WORD, ft_strndup(temp, cursor)));
+			add_back(&head, new_token(WORD, ft_strndup(temp, cursor)));
 			i += cursor - 1;
 		}
+		else
+			add_back(&head, new_token(UNKNOWN, ft_strndup(&str[i], 1)));
 	}
 	return (head);
 }
-
 
 int main(int argc, char const *argv[])
 {
 	t_token *tokens;
 	t_token	*temp;
 
-	tokens = get_tokens("cat | &&   |cat");
+	tokens = get_tokens("<< echo 123 | >> -f |grep ;include");
 	temp = tokens;
 	while (temp)
 	{
