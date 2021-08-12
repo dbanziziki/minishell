a935447e981f4394cee7c136838af7f87f089830
David Banziziki <dbanzizi@mi-r3-p4.s19.be>
2021-08-12 14:56:57 +0200

    shorter fun

--- srcs/minishell/minishell.c
+++ srcs/minishell/minishell.c
@@ -2,16 +2,43 @@
 #include "libft.h"
 #include <stdio.h>
 
-
-void	minishell()
+char	*ms_readline()
 {
-	t_minishell	*ms;
+	char	*line;
+	char	curr_dir[1024];
+	char	*dir;
+
+	getcwd(curr_dir, 1024);
+	dir = ft_strjoin_sep(curr_dir, "> ", '-');
+	line = readline(dir);
+	free(dir);
+	return (line);
 }
 
-int	run_process(t_minishell *ms, t_cmd *cmd, t_AST *ast)
+int	post_child_process(t_minishell *ms, t_cmd *cmd, t_AST *ast)
 {
 	int	i;
 
+	i = -1;
+	close_main_pipes(ms->pipes, ms->nb_proc);
+	while (++i < ms->nb_proc)
+		waitpid(ms->p_ids[i], NULL, 0);
+	/* free all the allocated memory */
+	free(ms->p_ids);
+	if (ms->pipes)
+	{
+		for (int i = 0; i < ms->nb_proc; i++)
+			free(ms->pipes[i]);
+	}
+	free(ms->pipes);
+	free_all(ms->p, ms->ast);
+	return (0);
+}
+
+int	run_process(t_minishell *ms, t_AST *ast)
+{
+	int		i;
+	t_cmd	*cmd;
 	i = -1;
 	while (++i < ms->nb_proc)
 	{
@@ -30,74 +57,42 @@ int	run_process(t_minishell *ms, t_cmd *cmd, t_AST *ast)
 		}
 		ast = ast->next; /* advence to the next cmd */
 	}
+	post_child_process(ms, cmd, ast);
 	return (0);
+}
+
+void	parse_line(t_minishell **ms, char *line)
+{
+	t_minishell *temp;
+	t_program	*prog;
+	t_AST		*ast;
+
+	temp = *ms;
+	temp->ast = init_minishell_parse(ms, line);
+	prog = (t_program *)temp->ast->body;
+	temp->nb_proc = prog->nb_pipes + 1;
+	temp->pipes = init_pipes(temp->nb_proc + 1);	
+	temp->p_ids = (pid_t *)malloc(sizeof(pid_t) * temp->nb_proc);
+	if (!temp->pipes || !temp->p_ids)
+		return ;
 }
 
 int main(int argc, char const *argv[])
 {
 	t_minishell	*ms;
 	char		*line;
-	t_cmd		*cmd;
 	t_AST		*ast;
-	int			nb_proc;
-	pid_t		pid;
-	t_program	*prog;
-	char		curr_dir[1024];
-	char		*dir;
 
 	ms = init_minishell_struct();
 	if (!ms)
 		return (1);
 	while (1)
 	{
-		getcwd(curr_dir, 1024);
-		dir = ft_strjoin_sep(curr_dir, "> ", '-');
-		#ifdef __APPLE__
-		line = readline(dir);
-		#else
-		get_next_line(STDIN_FILENO, &line);
-		#endif
-		ms->ast = init_minishell_parse(&ms, line);
-		prog = (t_program *)ms->ast->body;
-		nb_proc = prog->nb_pipes + 1;
-		ms->pipes = init_pipes(nb_proc + 1);
-		ms->p_ids = (pid_t *)malloc(nb_proc * sizeof(pid_t));
-		ms->nb_proc = nb_proc;
+		line = ms_readline();
+		parse_line(&ms, line);
 		ast = ms->ast->next; /* the first cmd to run */
-		//run_process(ms, cmd, ast);// for (int i = 0; i < nb_proc; i++)
-		for (int i = 0; i < nb_proc; i++)
-		{
-			ms->p_ids[i] = fork();
-			if (ms->p_ids[i] == -1)
-				return (1);
-			if (ms->p_ids[i] == 0) /* child process */
-			{
-				/* closing all the unused pipes */
-				close_unused_pipes(ms->pipes, nb_proc, i);
-				cmd = (t_cmd *)ast->body;
-				cmd->proc_idx = i;
-				cmd_and_args(ms, ast, cmd);
-				exit(EXIT_FAILURE);
-				return (0); /* to avoid that the child process runs the for loop */
-			}
-			ast = ast->next; /* advence to the next cmd */
-		}
-		/* only the main process runs here */
-		/* close all the pipes in the main process */
-		close_main_pipes(ms->pipes, nb_proc);
-		for (int i = 0; i < nb_proc; i++)
-			waitpid(ms->p_ids[i], NULL, 0);
-		/* free all the allocated memory */
+		run_process(ms, ast);
 		free(line);
-		free(ms->p_ids);
-		if (ms->pipes)
-		{
-			for (int i = 0; i < nb_proc + 1; i++)
-				free(ms->pipes[i]);
-		}
-		free(ms->pipes);
-		free(dir);
-		free_all(ms->p, ms->ast);
 	}
 	return 0;
 }
